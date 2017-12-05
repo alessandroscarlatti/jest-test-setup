f8be756ab4aec43bd391f22509caf81390af957f
alessandroscarlatti <violanotesnoextras@gmail.com>
2017-12-04 22:55:56 -0600

    ammended failing test

--- Utils.test.js
+++ Utils.test.js
@@ -4,6 +4,6 @@ test("adding 1 + 2 gives 3", () => {
 	expect(Utils.add(1, 2)).toBe(3);
 });
 
-test("adding 2 + 2 gives 5", () => {
-	expect(Utils.add(2, 2)).toBe(5);
+test("adding 2 + 2 gives 4", () => {
+	expect(Utils.add(2, 2)).toBe(4);
 });
